79b270f24f730224e7ada531bc35cd2dc1de8f0e
likuraNT <claterfunk@gmail.com>
2025-02-25 10:17:58 +0300

    Final vers

--- functions/main.c
+++ functions/main.c
@@ -1,10 +1,14 @@
 #define _GNU_SOURCE
 
-#include "usr.c"
-#include "prcs.c"
-#include "help.c"
+#include "usr.h"
+#include "prcs.h"
+#include "help.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <getopt.h>
+
+
 
 int main(int argc, char* argv[])
 {
@@ -15,14 +19,15 @@ int main(int argc, char* argv[])
         { "users",          no_argument, NULL, 'u'},
         { "processes",      no_argument, NULL, 'p'},
         { "help",           no_argument, NULL, 'h'},
-        { "log",            optional_argument, NULL, 'l'},
-        { "errors",         optional_argument, NULL, 'e'},
+        { "log",            required_argument, NULL, 'l'},
+        { "errors",         required_argument, NULL, 'e'},
         { NULL,             0,                 NULL,   0}
     };
 
     FILE* out = stdout, *err = stderr;
     int result = 0;
     int option_ind = 0;
+    int output_result[5];
 
     if (argc == 1)
     {
@@ -36,30 +41,68 @@ int main(int argc, char* argv[])
         {
             case 'u':
             {
+                output_result[4] = 1;
                 show_user_list(out, err);
                 break;
             }
             case 'p':
             {
+                output_result[3] = 1;
                 show_processes_list(out, err);
                 break;
             }
             case 'h':
             {
+                output_result[2] = 1;
                 show_help(out, err);
                 break;
             }
             case 'l':
             {
-                if (( out = fopen(optarg, "w")) == NULL)
+                output_result[1] = 1;
+                break;
+            }
+            case 'e':
+            {
+                output_result[0] = 1;
+                break;
+            }
+            default:
+            {
+                perror("Error parsing arguments");
+                exit(-1);
+                break;
+            }
+            return 0;
+        }
+        if (output_result[1] == 1)
+        {
+            if ((out = fopen(optarg, "w+")) == NULL)
                 {
                     perror("Error opening log file");
                     exit(-1);
                 }
-                break;
+
+            else if (output_result[4] == 1) {show_user_list(out, err);}
+            else if (output_result[3] == 1) {show_processes_list(out, out);}
+            else if (output_result[2] == 1) {show_help(out, err);}
+        }
+
+        if (output_result[0] == 1)
+        {
+            if ((err = fopen(optarg, "w+")) == NULL)
+            {
+                perror("Error opening err file");
+                exit(-1);
             }
+
+            else if (output_result[4] == 1) {show_user_list(err, err);}
+            else if (output_result[3] == 1) {show_processes_list(err, err);}
+            else if (output_result[2] == 1) {show_help(err, err);}
+
         }
     }
     fclose(out);
     fclose(err);
+    return 0;
 }
